04dbb6951a62d063606ffbec1ddd98a6aa6078b3
Rui <rui@dhcp154.merusouth127.iit.edu>
2017-08-14 15:45:06 -0500

    first commit

---
+++ src/Solution.java
@@ -0,0 +1,25 @@
+
+public class Solution {
+	public static int[] twoSum(int[] nums, int target) {
+		int [] sumArray = new int[2];
+		int l = nums.length;
+		for(int i = 0; i < nums.length; i++){
+			for(int j = 0; j < nums.length; j++){
+				if(i == j){
+					continue;
+				}else{
+					int sum = nums[i] + nums[j];
+					if(sum == target){
+						sumArray[0] = i;
+						sumArray[1] = j;
+						break;
+					}
+				}
+			}
+		}
+		
+		return sumArray;
+	}  
+  
+}
+
